35dac99ee9ab8756e84d396aa8357d9f4de6ff3d
Vincent Crame <vincentcrame7@gmail.com>
2026-04-05 09:59:06 +0800

    add: log in modal

---
+++ app/Views/users/login.php
@@ -0,0 +1,31 @@
+<?php require_once '../app/Views/templates/header.php'; ?>
+<div class="row">
+    <div class="col-md-6 mx-auto">
+        <div class="card card-body bg-light mt-5">
+            <?php flash('register_success'); ?>
+            <h2>Login</h2>
+            <p>Please fill in your credentials to log in</p>
+            <form action="<?= URLROOT; ?>/users/login" method="POST">
+                <div class="form-group mb-3">
+                    <label for="email">Email <span class="text-danger">*</span></label>
+                    <input type="email" name="email" class="form-control form-control-lg <?= (!empty($data['email_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['email']; ?>">
+                    <span class="invalid-feedback"><?= $data['email_err']; ?></span>
+                </div>
+                <div class="form-group mb-3">
+                    <label for="password">Password <span class="text-danger">*</span></label>
+                    <input type="password" name="password" class="form-control form-control-lg <?= (!empty($data['password_err'])) ? 'is-invalid' : ''; ?>" value="<?= $data['password']; ?>">
+                    <span class="invalid-feedback"><?= $data['password_err']; ?></span>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <input type="submit" value="Login" class="btn btn-success btn-block w-100">
+                    </div>
+                    <div class="col-md-6">
+                        <a href="<?= URLROOT; ?>/users/register" class="btn btn-light btn-block w-100">No account? Register</a>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+<?php require_once '../app/Views/templates/footer.php'; ?>
