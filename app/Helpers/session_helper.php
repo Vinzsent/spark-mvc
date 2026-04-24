739004caed8c37656f6ec77823286bc383b3d45b
Vincent Crame <vincentcrame7@gmail.com>
2026-04-24 19:56:46 +0800

    add: log in message

--- app/Helpers/session_helper.php
+++ app/Helpers/session_helper.php
@@ -2,7 +2,7 @@
 // Flash message helper
 // EXAMPLE - flash('register_success', 'You are now registered');
 // DISPLAY IN VIEW - echo flash('register_success');
-function flash($name = '', $message = '', $class = 'alert alert-success'){
+function flash($name = '', $message = '', $class = 'bg-emerald-50 border border-emerald-200 text-emerald-700 px-4 py-4 rounded-2xl text-sm mb-6 shadow-sm flex items-center animate-in fade-in slide-in-from-top-4 duration-500'){
     if(!empty($name)){
         if(!empty($message) && empty($_SESSION[$name])){
             if(!empty($_SESSION[$name])){
@@ -17,7 +17,12 @@ function flash($name = '', $message = '', $class = 'alert alert-success'){
             $_SESSION[$name. '_class'] = $class;
         } elseif(empty($message) && !empty($_SESSION[$name])){
             $class = !empty($_SESSION[$name. '_class']) ? $_SESSION[$name. '_class'] : '';
-            echo '<div class="'.$class.'" id="msg-flash">'.$_SESSION[$name].'</div>';
+            echo '<div class="'.$class.'" id="msg-flash">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                    </svg>
+                    <span class="font-bold">'.$_SESSION[$name].'</span>
+                  </div>';
             unset($_SESSION[$name]);
             unset($_SESSION[$name. '_class']);
         }
